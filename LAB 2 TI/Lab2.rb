396399f9101f0b9152a22e470d0858d15b83b274
Stanislav Berkovich <stanislav.berkovich@gmail.com>
2014-10-15 23:21:05 +0300

    Comlete 2 lab ti

--- LAB 2 TI/Lab2.rb
+++ LAB 2 TI/Lab2.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'gruff'
 
 class Pair
   attr_reader :x, :y
@@ -16,7 +17,7 @@ class BinDistribution
   attr_reader :alpha, :beta, :px, :py
 
   def initialize px, alpha, beta
-    @alpha, @beta, @px = px, alpha, beta
+    @alpha, @beta, @px = alpha, beta, px
     @py = [[@alpha, 1 - @alpha],
            [1- @beta, @beta]]
   end
@@ -55,12 +56,12 @@ class Sequence
 end
 
 class Lab2
-  attr_reader :seq
+  attr_reader :seq, :alpha, :beta, :px
 
   COUNT_FOR_VAR_SERIES = 10000
 
   def initialize t, px, alpha, beta
-    @px, @alpha, @beta, @dist =  px, alpha, beta, BinDistribution.new(px, alpha, beta)
+    @px, @alpha, @beta, @dist = px, alpha, beta, BinDistribution.new(px, alpha, beta)
     @seq = Sequence.new t, @dist
   end
 
@@ -69,26 +70,46 @@ class Lab2
   end
 
   def entropy_Y_X
-    (1-@px)*(nu(@alpha)+nu(1-@alpha)) + @px*(nu(@beta)+nu(1-@beta))
+    entropy(@dist.py.flatten)
   end
 
   def entropy_Y_XplY
-    entropy_XY - entropy_XplY
+    entropy_YXplY - entropy_XplY
   end
 
   def empirical_entropy_X_Y
-    pair_freq, freq_y = count_pair_freq, count_freq_of_one(:y)
-    freq_y[1] * (nu(pair_freq[0]) + nu(pair_freq[2])) + freq_y[0] * (nu(pair_freq[1]) + nu(pair_freq[3]))
+    entropy(count_pair_freq) - entropy(count_freq_of_one(:y))
   end
 
   def empirical_entropy_Y_X
-    pair_freq, freq_x = count_pair_freq, count_freq_of_one(:x)
-    freq_x[0] * (nu(pair_freq[0]) + nu(pair_freq[1])) + freq_x[1] * (nu(pair_freq[2]) + nu(pair_freq[3]))
+    entropy(count_pair_freq) - entropy(count_freq_of_one(:x))
   end
 
   def empirical_entropy_Y_XplY
-    sum_freq, freq_y = count_freq_XplY, count_freq_of_one(:y)
-    #TODO: Дописать
+    entropy(count_freq_YXplY) - entropy(count_freq_XplY)
+  end
+
+  def graph_family
+    for_params = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    res = {10**3 => {}, 10**5 => {}}
+    puts "Graph family"
+    [10**3, 10**5].each do |t|
+      for_params.each do |alpha|
+        es, ems, xs = [], [], []
+        for_params.each do |beta|
+          @dist, @alpha, @beta, @px = BinDistribution.new(0.5, alpha, beta), alpha, beta, 0.5
+          @seq = Sequence.new t, @dist
+          if (alpha > 0.5 && beta >= 1-alpha) || (alpha <= 0.5 && beta <= 1-alpha)
+            es << entropy_Y_X
+            ems << empirical_entropy_Y_X
+            xs << (1-alpha - beta).abs.round(1)
+            res[t][(1-alpha - beta).abs.round(1)] = {'entropy' => entropy_Y_X, 'empirical_entropy' => empirical_entropy_Y_X}
+          end
+        end
+        draw_gruff(title: "alpha = #{alpha}", xs: xs, data: [['Entropy', es], ['Empirical Entropy', ems]])
+      end
+    end
+    res
   end
 
   def graph_info
@@ -98,6 +119,7 @@ class Lab2
       vars_Y_X << var_series_Y_X(size, entr_Y_X)
       puts "Size: #{size}, var_X_Y: #{vars_X_Y.last}, var_Y_X: #{vars_Y_X.last}"
     end
+    draw_gruff({title: "Alpha = #{@alpha}, Beta = #{@beta}, Pi1 = #{@px}", xs: ts, data: [['V(H{x1|x2})', vars_X_Y], ['V(Hemp{x2|x1})', vars_Y_X]]})
   end
 
   private
@@ -124,48 +146,107 @@ class Lab2
 
   def count_pair_freq
     pair_freq = Array.new(4, 0.0)
-    @seq.arr.each { |pair|  pair_freq[2 * pair.x + pair.y] += 1 }
+    @seq.arr.each { |pair| pair_freq[2 * pair.x + pair.y] += 1 }
     pair_freq.map { |p| p / @seq.length }
   end
 
-
   def count_freq_XplY
-    freq = Array.new(3,0.0)
+    freq = Array.new(3, 0.0)
     @seq.arr.each { |pair| freq[pair.x + pair.y] += 1 }
     freq.map { |p| p / @seq.length }
   end
 
+  def count_freq_YXplY
+    freq = Array.new(6, 0.0)
+    @seq.arr.each do |pair|
+      index = 2 * pair.y + (pair.y == 0 ? pair.x : pair.x + 1)
+      freq[index] += 1
+    end
+    freq.map { |p| p / @seq.length }
+  end
+
   def count_freq_of_one p
-    freq_p = Array.new(2,0.0)
+    freq_p = Array.new(2, 0.0)
     @seq.arr.each { |pair| freq_p[pair.send(p)] += 1 }
     freq_p.map { |p| p / @seq.length }
   end
 
   def nu p
-    p == 0 ? 0 : -p * Math.log2(p)
+    p == 0 ? 0 : Math.log2(p)
+  end
+
+  def entropy_YXplY
+    entropy [((1 - @px) * @alpha + @px * (1 - @beta)) * (1 - @px), ((1 - @px) * @alpha + @px * (1 - @beta)) * @px, 0,
+             0, ((1 - @px) * (1 - @alpha) + @px * @beta) * (1-@px), ((1 - @px) * (1 - @alpha) + @px * @beta) * @px]
+
+  end
+
+  def entropy arr
+    arr.inject(0) do |entropy, p|
+      entropy - p * nu(p)
+    end
   end
 
   def entropy_XY
-    nu((1 - @px) * @alpha) + nu( @px * (1 - @beta) ) + nu(( 1 - @px) * (1 - @alpha)) + nu(@px * @beta)
+    unless @entropyXY
+      @entropyXY = entropy([@px, 1-@px]) + entropy_Y_X
+    else
+      @entropyXY
+    end
   end
 
   def entropy_Y
-    nu((1 - @px) * @alpha + @px * (1 - @beta)) + nu((1 - @px) * (1 - @alpha) + @px * @beta)
+    unless @entropyY
+      @entropyY = entropy([@alpha * (1 - @px) + @px * (1-@beta), @beta * @px + (1-@alpha) * (1-@px)])
+    else
+      @entropyY
+    end
   end
 
   def entropy_XplY
-    nu((1 - @px) * @alpha) + nu(@px * (1 - @beta) + (1 - @px) * (1 - @alpha)) + nu(@px * @beta)
+    unless @entropyXplY
+      @entropyXplY = entropy([@px * @alpha, @px *(1- @beta) + (1 - @px) *(1- @alpha), @px * @beta])
+    else
+      @entropyXplY
+    end
   end
+
+  def draw_gruff hash
+    g = Gruff::Line.new
+    g.title = hash[:title]
+    g.labels = to_get_keys(hash[:xs])
+    hash[:data].each do |data|
+      g.data data[0], data[1]
+    end
+    g.write("#{hash[:title]}.png")
+  end
+
+  def to_get_keys arr
+    res = {}
+    arr.sort.each_with_index { |a, i| res[i] = a.to_s[0...3] }
+    res
+  end
+
 end
 
 
+#  data = [[0.3, 0.8, 0.5], [0.7, 0.2, 0.6], [0.3, 0.5, 0.4], [0.4, 0.5, 0, 6]]
+#  data.each do |d|
+#     l = Lab2.new 20, d[0], d[1], d[2]
+#    puts "Alpha = #{l.alpha}, Beta = #{l.beta}, Pi1 = #{l.px}"
+#  puts 'Sequence:'
+#   l.seq.arr.each do |a|
+#     print "#{a}, "
+#   end
+#   puts
+#   puts "Entropy X | Y : ", l.entropy_X_Y
+#   puts "Entropy Y | X : ", l.entropy_Y_X
+#   puts "Empiric Entropy X | Y : ", l.empirical_entropy_X_Y
+#   puts "Empiric Entropy Y | X : ", l.empirical_entropy_Y_X
+#    puts "Entropy Y | X+Y : ", l.entropy_Y_XplY
+#    puts "Empiric Entropy Y | X+Y", l.empirical_entropy_Y_XplY
+#   l.graph_info
 
-
-alpha, beta, px = 0.3, 0.5, 0.7
-l = Lab2.new 20, px, alpha, beta
-puts l.seq.arr
-puts "Entropy X | Y : ", l.entropy_X_Y
-puts "Entropy Y | X : ", l.entropy_Y_X
-puts "Empiric Entropy X | Y : ", l.empirical_entropy_X_Y
-puts "Empiric Entropy Y | X : ", l.empirical_entropy_Y_X
-l.graph_info
+# end
+l = Lab2.new 20, 1, 0.2, 0.4
+l.graph_family
