accf9cba628a2743989d6e462d38d91c89be41c5
Stanislav <stanislav.berkovich@gmail.com>
2014-11-14 00:36:00 +0200

    cHange 3 lab ti

--- LAB 3 TI/src.rb
+++ LAB 3 TI/src.rb
@@ -12,7 +12,6 @@ class MarkovGen
 	end
 
 	def teor_udel_entropy
-		# entropy(@p_s) + ( - @extent) * entropy(@p.flatten @extent)
 		entropy(@p.flatten @extent)
 	end
 
@@ -20,7 +19,47 @@ class MarkovGen
 		entropy(count_freq(seq).flatten @extent)
 	end
 
+	def graphic
+		es, ems, ts = [], [], [10, 100, 1000, 10**4, 10**5, 10*6, 10**7, 10**8]
+		ts.each do |t|
+			seq = generate_sequence t: t
+			ems << emperical_udel_entropy seq
+		end
+		tue = teor_udel_entropy
+		ts.length.times { es << tue		}
+		draw_gruff(title: "Alphabet power: #{alpha_count}", xs: ts, data: [['Real', es], ['Empirical', ems]])
+	end
+
+	def graph_family
+    for_params = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    puts "Graph family"
+    [10**3, 10**5].each do |t|
+      for_params.each do |alpha|
+        es, ems, xs = [], [], []
+        for_params.each do |beta|
+          @dist, @alpha, @beta, @px = BinDistribution.new(0.5, alpha, beta), alpha, beta, 0.5
+          @seq = Sequence.new t, @dist
+          if (alpha > 0.5 && beta >= 1-alpha) || (alpha <= 0.5 && beta <= 1-alpha)
+            es << entropy_Y_X
+            ems << empirical_entropy_Y_X
+            xs << (1-alpha - beta).abs.round(1)
+          end
+        end
+        
+      end
+    end
+
 	private
+
+	def draw_gruff hash
+    g = Gruff::Line.new
+    g.title = hash[:title]
+    g.labels = to_get_keys(hash[:xs])
+    hash[:data].each do |data|
+      g.data data[0], data[1]
+    end
+    g.write("#{hash[:title]}.png")
+  end
 
 	def generate_start
 		v, f= rand, 0
@@ -105,6 +144,17 @@ end
 # 																																								[[0.2, 0.7, 0.1], [0.4, 0.1, 0.5], [0.6, 0.2, 0.2]],
 # 																																								[[0.7, 0.1, 0.2], [0.2, 0.6, 0.2], [0.5, 0.3, 0.2]]] }
 #алфавит 2, степень 2 																																								
-m = MarkovGen.new 2, {pi: [0.2, 0.3, 0.4, 0.1], cube: [[[0.3, 0.7], [0.5, 0.5]], [[0.2, 0.8], [0.6, 0.4]]]}
-puts m.teor_udel_entropy
-puts m.emperical_udel_entropy m.generate_sequence t: 1000000
+# m = MarkovGen.new 2, {pi: [0.2, 0.3, 0.4, 0.1], cube: [[[0.3, 0.7], [0.5, 0.5]], [[0.2, 0.8], [0.6, 0.4]]]}
+
+
+
+[MarkovGen.new(2, {pi: [0.2, 0.3, 0.4, 0.1], cube: [[[0.3, 0.7], [0.5, 0.5]], [[0.2, 0.8], [0.6, 0.4]]]}),
+  MarkovGen.new(2, {pi: [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 ], cube: [[[0.3, 0.2, 0.5], [0.5, 0.1, 0.4], [0.8, 0.1, 0.1]], 
+																																								[[0.2, 0.7, 0.1], [0.4, 0.1, 0.5], [0.6, 0.2, 0.2]],
+																																								[[0.7, 0.1, 0.2], [0.2, 0.6, 0.2], [0.5, 0.3, 0.2]]] }),
+  MarkovGen.new(1, {pi: [0.5, 0.3, 0.2 ], mat: [[0.3, 0.2, 0.5], [0.5, 0.1, 0.4], [0.8, 0.1, 0.1]]}), 
+  MarkovGen.new 1, {pi: [0.4, 0.6], mat: [[0.3, 0.7], [0.8, 0.2]]}].each do |m|	
+    puts m.teor_udel_entropy
+    puts m.emperical_udel_entropy m.generate_sequence t: 100
+    m.graphic
+  end
